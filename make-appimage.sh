5b7985970988a53e1275e9c37ab9debd3490c646
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-18 20:35:45 -0400

    set `APPNAME`

--- make-appimage.sh
+++ make-appimage.sh
@@ -11,6 +11,7 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=DUMMY
 export DESKTOP=DUMMY
 export MAIN_BIN=nsz
+export APPNAME=NSZ
 export DEPLOY_OPENGL=1
 export DEPLOY_PYTHON=1
 
