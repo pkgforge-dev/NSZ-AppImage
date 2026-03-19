45a9b0e2ae72ffef56aa332f8f36d79b73c5ba57
Samueru-sama <xdglawyer@outlook.com>
2026-03-18 20:27:28 -0400

    update to latest template

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,36 +1,26 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+ARCH=$(uname -m)
 
-echo "Installing build dependencies..."
+echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm \
-	base-devel        \
-	curl              \
-	fontconfig        \
-	freetype2         \
-	git               \
-	libxcb            \
-	libxcursor        \
-	libxi             \
-	libxkbcommon      \
-	libxkbcommon-x11  \
-	libxrandr         \
-	libxtst           \
-	libxss            \
-	mtdev             \
-	pulseaudio        \
-	pulseaudio-alsa   \
-	sdl2              \
-	wayland           \
-	wget              \
-	xorg-server-xvfb  \
-	zsync
+pacman -Syu --noconfirm nsz
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano
+
+# Comment this out if you need an AUR package
+#make-aur-package PACKAGENAME
+
+# If the application needs to be manually built that has to be done down here
+
+# if you also have to make nightly releases check for DEVEL_RELEASE = 1
+#
+# if [ "${DEVEL_RELEASE-}" = 1 ]; then
+# 	nightly build steps
+# else
+# 	regular build steps
+# fi
---
+++ make-appimage.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -eu
+
+ARCH=$(uname -m)
+VERSION=$(pacman -Q nsz | awk '{print $2; exit}') # example command to get version of application here
+export ARCH VERSION
+export OUTPATH=./dist
+export ADD_HOOKS="self-updater.bg.hook"
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=DUMMY
+export DESKTOP=DUMMY
+export MAIN_BIN=nsz
+export DEPLOY_OPENGL=1
+export DEPLOY_PYTHON=1
+
+# Deploy dependencies
+quick-sharun /usr/bin/nsz
+
+# Additional changes can be done in between here
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
+
+# Test the app for 12 seconds, if the test fails due to the app
+# having issues running in the CI use --simple-test instead
+quick-sharun --simple-test ./dist/*.AppImage
